89349749827071d00fe9259f21d6a7276ec393d0
Abdullah Karagöz <abdullahkaragoz30@gmail.com>
2022-03-15 00:54:11 +0300

    - Veri Çekmek

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -6,21 +6,67 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    var isimDizisi = [String]()
+    var idDizisi = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.delegate = self
+        tableView.dataSource = self
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(eklemeButtonuTiklandi))
+        
+        verileriAl()
+    }
+    
+
+    func verileriAl(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alisveris")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let sonuclar = try context.fetch(fetchRequest)
+            
+            for sonuc in sonuclar as! [NSManagedObject]{
+                
+                if let isim = sonuc.value(forKey: "isim") as? String {
+                    isimDizisi.append(isim)
+                }
+                
+                if let id = sonuc.value(forKey: "id") as? UUID {
+                    idDizisi.append(id)
+                }
+                
+            }
+            tableView.reloadData()
+        } catch {
+            
+        }
+        
     }
     
    @objc func eklemeButtonuTiklandi() {
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isimDizisi.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = isimDizisi[indexPath.row]
+        return cell
+    }
 
 }
 
